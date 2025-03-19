85bf6fe67d2e66e296eef211f65b006d29db3c4d
Om <solankiom2507@gmail.com>
2025-03-19 14:06:09 -0400

    About me changes

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -1,24 +1,21 @@
 export default function AboutMe() {
   return (
     <section id="AboutMe" className="about--section">
-      <div className="about--section--img">
+      {/* <div className="about--section--img">
         <img src="./img/about-me.png" alt="About Me" />
-      </div>
+      </div> */}
       <div className="hero--section--content--box about--section--box">
         <div className="hero--section--content">
-          <p className="section--title">About</p>
+          {/* <p className="section--title">About</p> */}
           <h1 className="skills-section--heading">About Me</h1>
           <p className="hero--section-description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
-            officiis sit debitis omnis harum sed veniam quasi dicta accusamus
-            recusandae? Voluptatem, reprehenderit alias? Eligendi aperiam
-            tempora numquam sint odit optio.
+            I am Omkumar Solanki, a highly motivated and detail-oriented Software Engineer with a passion for building scalable and efficient applications. With a strong background in full-stack development and cloud computing, I specialize in designing and deploying robust web applications and backend systems.
           </p>
-          <p className="hero--section-description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
-            officiis sit debitis omnis harum sed veniam quasi dicta accusamus
-            recusandae?
-          </p>
+
+          <p className="hero--section-description">At MAQ Software, I worked extensively with C#, .NET, JavaScript, and SQL, developing full-stack web applications that improved efficiency and scalability. I was also responsible for developing, deploying and managing applications, ensuring seamless integration and performance.</p>
+          <p className="hero--section-description">Beyond professional experience, I built a Cloud Infrastructure Project using Google Cloud Platform (GCP), Terraform, Packer, and GitHub Actions. The project involved designing and deploying a scalable Node.js web application backend with serverless architecture, auto-scaling, and CI/CD pipelines, enhancing deployment efficiency and reliability.</p>
+          <p className="hero--section-description">Additionally, I contributed to medical software development, working on an iOS heart rate monitoring app that complies with IEC 62304 standards. The app integrates with the Polar H9 sensor, providing real-time heart rate tracking and secure data export for healthcare professionals.</p>
+          <p className="hero--section-description">I thrive on solving complex problems, optimizing performance, and leveraging modern technologies to build impactful solutions. Let’s connect and create something innovative!</p>
         </div>
       </div>
     </section>
